832cbddab32184a4e4af311fc956155925518387
3akare <bakaredavid007@gmail.com>
2022-06-05 12:39:35 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'alphabet game'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	while (a = 1;a >= 'z';)
+	{
+		putchar(a);
+		a++
+	}
+	putchar(a\n);
+	return (0);
+}
