5d25f5b3480973763b182ebfa5ca4dc3752f131d
3akare <bakaredavid007@gmail.com>
2022-06-06 03:43:35 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print the aplhabet excluding e and q'
+ *
+ * Retrun: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		if (alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+	putchar('\n');
+	return (0);
+}
