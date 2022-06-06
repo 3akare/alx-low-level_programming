d9c559e59dd25f6d0502b078192e37a2cecf6390
3akare <bakaredavid007@gmail.com>
2022-06-06 04:05:32 -0700

    print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -13,8 +13,8 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 11; number++)
-		putchar(number);
+	for (number = 0; number <= 10; number++)
+		printf("%d", number);
 	putchar('\n');
 
 	return (0);
