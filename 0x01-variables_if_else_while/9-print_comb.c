34c0d6137a7f38ba4d7dde6b488de2afc9da7f1a
3akare <bakaredavid007@gmail.com>
2022-06-06 06:49:18 -0700

    print all possible combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print all possible combinations of single-digits numbers'
+ *
+ * Retrun: Always 0 (Success)
+ */
+
+int main(void)
+{
+	return (0);
+}
