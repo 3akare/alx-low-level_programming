320056d2897076585a53e9b747fbe6827e33e5c1
3akare <bakaredavid007@gmail.com>
2022-06-06 06:55:18 -0700

    print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,10 +5,17 @@
  *
  * Description: 'print all possible combinations of single-digits numbers'
  *
- * Retrun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	char number;
+
+	for  (number = '0'; number <= '9'; number++)
+		putchar(number);
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
 	return (0);
 }
