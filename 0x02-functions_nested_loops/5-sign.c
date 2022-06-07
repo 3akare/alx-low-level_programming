30bba1b4e5ebd0bab8ee80b30bd78738232167c7
3akare <bakaredavid007@gmail.com>
2022-06-07 14:52:49 -0700

    testing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, %d", n);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-, %d", n);
 		return (-1);
 	}
 	else
 	{
-		printf("0, %d", n);
 		return (0);
 	}
 }
