ee419b608a97a448ccdce98c8f729085f426afe5
3akare <bakaredavid007@gmail.com>
2022-06-07 15:01:02 -0700

    a function that prints out the value of an integer

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (+1);
+		putchar(43);
+		return (1);
 	}
 	else if (n < 0)
 	{
+		putchar(45);
 		return (-1);
 	}
 	else
 	{
+		putchar(48);
 		return (0);
 	}
+	putchar('\n');
 }
