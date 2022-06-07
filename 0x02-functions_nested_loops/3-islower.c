5ad1f2e6685ff71dc1e741932a0bb41395781f57
3akare <bakaredavid007@gmail.com>
2022-06-07 14:03:38 -0700

    testing

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * islower - a function that return 1 or 0 depending on the value of "c"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
