55972e7170e4a399ba7d3eb8b3313888f529f17d
3akare <bakaredavid007@gmail.com>
2022-06-07 13:28:36 -0700

    testing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * print_alphabet - prints out the alphabets
+ * print_alphabet_x10 - prints out the alphabets
  *
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 
 {
 	char alpha;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,3 @@
 int _putchar(char c);
 void print_alphabet(void);
-void alphabets(void);
+void print_alphabet_x10(void);
