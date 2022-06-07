cac38a141627a7a8caed6b1c63018fcc5462f579
3akare <bakaredavid007@gmail.com>
2022-06-07 14:10:57 -0700

    testin

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,20 @@
 #include "main.h"
 
 /**
- * test_islower - a function that return 1 or 0 depending on the value of "c"
+ * test_islower - a function that return 1 or 0 depending on the value of c
+ *
+ * @c: The character in ASCII code
  *
  * Return: Always 0 (Success)
  */
 
 int test_islower(int c)
 {
-	if (islower(c))
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
 	else
 		return (0);
+	putchar('\n');
 }
