94f4c78fd300730ecb27bf41be8fa12af2f9b8cd
3akare <bakaredavid007@gmail.com>
2022-06-07 10:59:17 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,12 @@
 
 /**
  * main - print _putchar
- * put - a function that prints the word "_putchar"
  *
  * Return: Always 0 (Success)
  */
 
-void put(void)
-{
-	printf("_putchar\n");
-}
 int main(void)
 {
-	put();
+	putchar("_putchar\n");
 	return (0);
 }
