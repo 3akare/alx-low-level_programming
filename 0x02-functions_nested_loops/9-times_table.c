a49b3b195bb728ab7ccbfa315f2465a82afce1bb
3akare <bakaredavid007@gmail.com>
2022-06-08 15:46:36 -0700

    testing

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,6 +2,13 @@
 #include <unistd.h>
 #include "main.h"
 
+
+/**
+ * times_table - a function that prints out the 9 times table
+ *
+ * Return: Always 0 (Success)
+ */
+
 void times_table(void)
 {
 	int i;
@@ -14,8 +21,11 @@ void times_table(void)
 			int u;
 
 			u = k * i;
-			printf("%d", u);
+			printf("%d,  ", u);
+			if (i < 9)
+				printf("%d", u);
 		}
+		putchar('\n');
 		k++;
 	}
 }
