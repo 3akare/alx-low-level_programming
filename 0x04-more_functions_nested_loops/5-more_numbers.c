165bb86114d825868b9aac20f66a85f5216b6fd5
3akare <bakaredavid007@gmail.com>
2022-06-09 13:25:01 -0700

    testing

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14, ten times
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int k = 0;
+	int l = 16;
+
+	while (k < 10)
+	{
+		for (i = 0; i < l; i++)
+			_putchar(i);
+		_putchar('\n');
+	_putchar('\n');
+	k++;
+	}
+}
