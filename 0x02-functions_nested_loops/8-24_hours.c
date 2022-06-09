29e10c0d76026e0835750dae31bef43b2f8bcfd3
David Bakare <98014597+3akare@users.noreply.github.com>
2022-06-09 01:50:15 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,12 @@
+include <stdio.h>
+include "main.h"
 
+/**
+ * jack_bauer - a function that prints out the hours in a day
+ *
+ * Return: Always 0 (Success)
+ */
+  
+void jack_bauer(void)
+{
+    
