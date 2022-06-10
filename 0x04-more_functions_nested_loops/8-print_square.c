d6f3c4d9b8d5e4b75a1cc7bc19eb9c4769fa6a22
3akare <bakaredavid007@gmail.com>
2022-06-09 17:42:56 -0700

    testign

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,7 +16,7 @@ void print_square(int size)
 	for (int i = 0; i <= size; i++)
 	{
 		for (int i = 0; i <= size; i++)
-			_putchar('#');
-		_putchar('\n');
+			putchar('#');
+		putchar('\n');
 	}
 }
