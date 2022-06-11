846e11513de860e294f038b379fe7318e02fd643
3akare <bakaredavid007@gmail.com>
2022-06-10 17:40:32 -0700

    printprint line

---
+++ 0x04-more_functions_nested_loops/print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a striaght line using the - character
+ * @n - is an integer
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int len;
+
+	if(n > 0)
+	{
+		for (len = 0; len < n: len++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
