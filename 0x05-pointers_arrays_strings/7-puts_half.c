841b1b5c784fea2a25f645c0bf3f248e2fa842f8
3akare <bakaredavid007@gmail.com>
2022-06-13 16:10:18 -0700

    testing

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,12 @@ void puts_half(char *str)
 
 	while (str[index++])
 		len++;
-	len = len / 2;
-	for (index = 0; index < len; index--)
-		_putchar(str[index--]);
+	if ((len % 2) == 0)
+		n = len / 2;
+	else
+		n = (len -1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
 	_putchar('\n');
 }
