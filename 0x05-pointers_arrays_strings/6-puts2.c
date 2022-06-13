7cb1915f94066ed6876a01140699ade6b7f37e70
3akare <bakaredavid007@gmail.com>
2022-06-13 15:44:15 -0700

    testing

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,17 @@
 
 /**
  * puts2 - a functions that prints a string
- * @str: a variable
- * Return: Always 0;
+ * @str: a string
  */
 
 void puts2(char *str)
 {
-	int len = 0, index;
+	int index = 0, len = 0;
 
 	while (str[index++])
 		len++;
 
-	for (index = 0; index < len; index = index + 2)
+	for (index = 0; index < len; index += 2)
 		_putchar(str[index]);
 	_putchar('\n');
 }
