c761ffc98cb3d056c685f2f129cf5a6fc577bcb6
3akare <bakaredavid007@gmail.com>
2022-06-14 15:30:55 -0700

    testing

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,7 +9,7 @@
  * Return: the pointer to dest
  */
 
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 	int i = 0;
 
@@ -18,5 +18,6 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
+
 	return (dest);
 }
