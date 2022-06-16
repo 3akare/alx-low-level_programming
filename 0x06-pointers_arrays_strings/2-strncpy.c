3c3bd694d431722070d99535632d9817dca21a1b
3akare <bakaredavid007@gmail.com>
2022-06-15 20:38:35 -0700

    testing

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - a function that concatenates two strings.
+ *
+ * @dest: a pointer to the string
+ * @src: a string to be appended to @dest
+ * @n: an integer
+ *
+ * Return: a pointer to the destination string @dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, len = 0;
+
+	while (src[index++])
+		len++;
+
+	for (index = 0; index < n && src[index]; index++)
+		dest[len++] = src[index];
+
+	for (index = len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
