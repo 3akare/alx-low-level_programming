04a6535bdeac0c9f18606e4126f9bd7df65f5eae
3akare <bakaredavid007@gmail.com>
2022-06-16 16:46:20 -0700

    testing

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes a string
+ * @s : a string
+ * Return: Always Success
+ */
+
+char *cap_string(char *s)
+{
+	int i, j = 0;
+	int len = strlen(s);
+
+	for (i = 0; i < len; i++)
+	{
+		if ((s[i] == 32 ||
+		s[i] == 46 ||
+		s[i] == '\n' ||
+		s[i] == '\t') &&
+		s[i + 1] >= 97 &&
+		s[i + 1] <= 122)
+			s[i + 1] = s[i + 1] - 32;
+	}
+	return (s);
+}
