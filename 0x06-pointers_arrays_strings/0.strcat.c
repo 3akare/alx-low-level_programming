054c1ab710cf7831987a9051b0831d668ec9f151
3akare <bakaredavid007@gmail.com>
2022-06-15 19:56:33 -0700

    testing

--- 0x06-pointers_arrays_strings/0.strcat.c
+++ 0x06-pointers_arrays_strings/0.strcat.c
@@ -3,23 +3,22 @@
 
 /**
  * _strcat - a function that concatenates two strings.
- * @dest: a string
- * @src: a string
  *
- * Return: Always Successful
+ * @dest: a pointer to the string
+ * @src: a string to be appended to @dest
+ *
+ * Return: a pointer to the destination string @dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int len = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i++])
-		len++;
+	while (dest[index++])
+		dest_len++;
 
-	for (i = 0; src[i]; i++)
-		dest[len++] = src[i];
-	dest[i] = '\0';
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index++];
 
 	return (dest);
 }
