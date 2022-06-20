d4ddb976d23c038b790ee220eef7919e54b1e0b8
3akare <bakaredavid007@gmail.com>
2022-06-20 15:02:08 -0700

    testing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ char _putchar(char *c);
 char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
+void *_memcpy(void *dest, const void *src, unsigned int n);
 
 #endif
