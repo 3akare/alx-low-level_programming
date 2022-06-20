71206327a27a12e05de8e4daad22f4331a50bc12
3akare <bakaredavid007@gmail.com>
2022-06-20 14:45:13 -0700

    testing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ void print_diagums(int *a, int size);
 char *_strpbrk(char *s, char *accept);
 char _putchar(char *c);
 char *_strstr(char *haystack, char *needle);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
