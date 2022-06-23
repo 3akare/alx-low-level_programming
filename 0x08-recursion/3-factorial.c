79341d9812404ed0a2d46b8135c3fc44fbd96a4b
3akare <bakaredavid007@gmail.com>
2022-06-22 17:58:52 -0700

    testing

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,8 +10,14 @@
 
 int factorial(int n)
 {
-	if (n <= 0)
+	int t = n;
+
+	if (n < 0)
 		return (-1);
-	else
-		return (n * factorial(n - 1));
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+	t*= factorial(n -1);
+
+	return (result);
 }
