734a09accd72ad80cd3790b51e667cf0370a5afa
3akare <bakaredavid007@gmail.com>
2022-06-25 18:43:23 -0700

    whatismyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * @argc: command line arguments count
+ * @argv: array of arguments in string format
+ *
+ * Return: Always Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
