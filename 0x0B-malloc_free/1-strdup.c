f97508ae4ad1680e11a731e0d16bfaf81a06872c
3akare <bakaredavid007@gmail.com>
2022-06-27 23:19:27 +0100

    relocated string space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a new string which is a duplicate
+ *
+ * @str: the string to be relocated
+ *
+ * Return: Always Success
+ */
+
+char *_strdup(char *str)
+{
+	int index, len = 0;
+	char *newstr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str; index++)
+		len++;
+
+	newstr = malloc((len + 1)*(sizeof(char)));
+
+	if (newstr == NULL)
+		return(NULL);
+
+	for (index = 0;str[index];index++)
+		newstr[index] = str[index];
+
+	newstr[len] = '\0';
+	return (newstr);
+}
