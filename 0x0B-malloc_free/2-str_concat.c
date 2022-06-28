15f968e500beb5c5fd2504ada873b3de71bd80c1
3akare <bakaredavid007@gmail.com>
2022-06-28 20:29:41 +0100

    testing

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,26 +12,26 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int len, index, con_index = 0;
-	char *con_cat;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
-		s1 = NULL;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = NULL;
+		s2 = "";
 
 	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
 
-	con_cat = malloc((len) * sizeof(char));
+	concat_str = malloc((len) * sizeof(char));
 
-	if (con_cat == NULL)
+	if (concat_str == NULL)
 		return (NULL);
 
 	for (index = 0; s1[index]; index++)
-		con_cat[con_index++] = s1[index];
+		concat_str[concat_index++] = s1[index];
 	for (index = 0; s2[index]; index++)
-		con_cat[con_index++] = s2[index];
+		concat_str[concat_index++] = s2[index];
 
-	return (con_cat);
+	return (concat_str);
 }
