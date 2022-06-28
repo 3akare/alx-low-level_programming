b11bd93633c914fed18bec7ae2aaafcfa02479fd
3akare <bakaredavid007@gmail.com>
2022-06-28 11:46:06 -0700

    main.h added

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void free_grid(int **grid, int height);
+char *create_array(unsigned int size, char c);
+char *_strdnup(char *str);
+char *str_concat(char *s1, char *s2);
+
+#endif
