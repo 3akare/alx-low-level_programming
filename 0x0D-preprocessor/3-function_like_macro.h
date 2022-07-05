981d8ffed243c5ba070e26e8dd5f74b20cb628ce
3akare <bakaredavid007@gmail.com>
2022-07-05 15:55:26 +0100

    testing

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MARCO_H__
-#define FUNCTION_LIKE_MARCO_H__
+#ifndef FUNCTION_LIKE_MARCO_H
+#define FUNCTION_LIKE_MARCO_H
 
-#define ABS(x) ((x) < 0 ? ((-1) * (-x)) : (x))
+#define ABS(x) ((x) < 0 ? ((-1) * (x)) : (x))
 
 #endif
