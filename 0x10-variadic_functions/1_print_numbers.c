78bda0abb5a62e057c63e8c9c852e465a6d5cc7e
3akare <bakaredavid007@gmail.com>
2022-07-07 14:01:54 -0700

    testing

--- 0x10-variadic_functions/1_print_numbers.c
+++ 0x10-variadic_functions/1_print_numbers.c
@@ -1,12 +1,13 @@
-#include " variadic_functions.h"
+#include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_numbers  - prints numbers, followed by a new line
  *
- * @seperator: seperates the strings
+ * @separator: seperates the strings
  * @n: the numeber of intergers passed
- * 
+ *
  * Return: always success
  */
 
@@ -15,13 +16,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nums;
 	unsigned int index;
 
+	va_start(nums, n);
+
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(nums, int));
 
-		if (index != (n - 1) && seperatoe != NULL)
-			printf("%s", seperator);
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
-	printf('\n');
+	printf("\n");
+
 	va_end(nums);
 }
