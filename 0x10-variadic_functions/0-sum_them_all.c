3723dc536487dfaa617b0c7f1ef5af73fc384a5a
3akare <bakaredavid007@gmail.com>
2022-07-07 23:17:40 +0100

    testing

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,12 +2,13 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - sums up all nu,bers
- * @n: an unsigned integer
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
  *
- * Return: Always success
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list nums;
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
- * prints_strings - prints strings
- * @separator: printds ou the separator
- * @n: the number of strings passed
- * 
- * Return: always success
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
+ *
+ * Description: If separator is NULL, it is not printed.
+ *              If one of the strings if NULL, (nil) is printed instead.
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	char *str;
 	unsigned int index;
+
 	va_start(strings, n);
 
 	for (index = 0; index < n; index++)
@@ -25,11 +27,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
+
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
-	printf("/n");
+	printf("\n");
 
 	va_end(strings);
 }
