6b673ee31b1408ac47891c8daa923ee9b3fd1f92
3akare <bakaredavid007@gmail.com>
2022-07-07 23:07:45 +0100

    message

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
 
-void prints_numbers(const char *seperator, const unsigned int n, ...);
+void prints_numbers(const char *separator, const unsigned int n, ...);
+void prints_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 
 #endif
