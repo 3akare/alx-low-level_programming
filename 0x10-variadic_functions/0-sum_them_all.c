13f585b752a374818cdb401857ad06705296f8d7
3akare <bakaredavid007@gmail.com>
2022-07-07 20:55:56 +0100

    add them all up

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums up all nu,bers
+ * @n: an unsigned integer
+ *
+ * Return: Always success
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (int i = 0; 1 < n; i++)
+		sum += va_arg(ptr, int);
+
+	va_end(ptr);
+	return (sum);
+}
