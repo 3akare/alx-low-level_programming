3188e786feca61e8c7ca5ee8d5535199a45993e8
3akare <bakaredavid007@gmail.com>
2022-07-24 15:37:54 -0700

    poppy

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type "struct"
+ * @d: structure of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: the dog owner's name
+ *
+ * Return: Always Successfully
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
