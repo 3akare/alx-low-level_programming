51fe0ec2f773511921f92c2a714688e02ef4ce89
3akare <bakaredavid007@gmail.com>
2022-07-24 15:41:34 -0700

    poppy

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
-
+#include "stdlib.h"
+#include "stddef.h"
 /**
  * init_dog - initializes a variable of type "struct"
  * @d: structure of a dog
@@ -12,7 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
