8abafce13407941fb8a8614de4a31d2f38cf9e45
3akare <bakaredavid007@gmail.com>
2022-07-24 12:05:39 -0700

    testing

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 
 unsigned int binary_to_uint(const char *b);
 int get_bit(unsigned long int n, unsigned int index);
+int get_endianness(void);
 void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned long int index);
 int clear_bit(unsigned long int *n, unsigned long int index);
