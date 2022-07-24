8b5a3cbed6f21399f65f7602d183c6d739360c6d
3akare <bakaredavid007@gmail.com>
2022-07-24 16:45:14 -0700

    testing

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * free_dog - frees dogs
+ * @d; the dog to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
