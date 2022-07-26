8fb95ebf1d457440e4cddf76367ac95654f525d7
3akare <bakaredavid007@gmail.com>
2022-07-25 19:53:35 -0700

    testing

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file
+ * @filename: the name of the new file
+ * @text_content: the content of the file
+ *
+ * Return: Always Success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, len = 0;
+	FILE *fptr;
+
+	if (filename == NULL)
+		return (-1);
+	else if (text_content == NULL)
+		text_content == "";
+	else
+	{
+		fptr = fopen(filename, "w");
+		fputs(text_content, fptr);
+		fclose(fptr);
+		return (1);
+	}
+}
