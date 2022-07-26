edc1a881e0a4ec4b1f9cf8d7f3f390ae88793a53
3akare <bakaredavid007@gmail.com>
2022-07-25 18:59:05 -0700

    testing

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,33 @@
 #include "main.h"
 
 /**
- * read_textfile - read a text file and prints it to the POSIX
- * standard output
+ * read_textfile - read a text fie and prints it to the STDOUT
+ * @filename: the file to be printed out
+ * @letters: the number of letters that should be printed
  *
- * @filename: the file to be copied
- * @letters: the length of characters to be printed out
- *
- * Return: The actual number of letters it could
- * read and print, or 0 if it fails
+ * Return: 0 if fails and number of printed charcters when success
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buf[letters];
-	int r = -1;
-	int in = open(filename, O_RDONLY);
-	if (in < 0)
+	FILE *fptr;
+	char c;
+	int i = 0;
+
+	if (filename == NULL)
 		return (0);
-	while (1)
+
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
+		return (0);
+
+	c = fgetc(fptr);
+	while	(c != EOF && i < letters)
 	{
-		r = read(in, buf, sizeof(buf));
-		if(r == -1 || r == 0)
-		{
-			break;
-		}
-		r = write(1, buf, r);
-		if (r == -1 || r == 0)
-		{
-			break;
-		}
+		putchar(c);
+		c = fgetc(fptr);
+		i++;
 	}
-	return (letters);
+	fclose(fptr);
+	return (i);
 }
