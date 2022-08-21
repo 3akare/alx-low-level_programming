c31e2c9a5a320e017f6d6429e55a0ad6ad427fb6
bakare <bakaredavid007@gmail.com>
2022-08-22 00:19:00 +0100

    task magic

--- 0x06-pointers_arrays_strings/just.c
+++ 0x06-pointers_arrays_strings/just.c
@@ -2,28 +2,47 @@
 #include <stdio.h>
 
 /**
- * main - check the code for
- *
- * Return: Always 0.
+ * main - magic.c
+ * Return: Always (0)
  */
+
 int main(void)
 {
-    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
-    char *p;
+	int m;
+	int a[5];
+	int *p;
 
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    return (0);
+    //in c when an integer is declared and an integer array follows, 6 bits of memory is given
+
+    //in c when an integer is declared and an char array follows, 15 bits of memory is given
+
+    //when a char is declared and a char array follows, 
+    //12 bits of memory is given
+
+    //when a char is declared and a int array follows, 
+    //9 bits of memory is given
+
+	// a[2] = 1024;
+	p = &m;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 4) = 98;
+	/* ...so that this prints 98\n */
+    // printf("n = %ld\n", sizeof(3));
+    printf("n = %p\n", (p + 5));
+    printf("m = %p\n", &m);
+    // printf("n = %d\n", *(p + sizeof(int) * ));
+	printf("a = %p\n", &a);
+	// printf("a[0] = %p\n", &a[0]);
+	// printf("a[1] = %p\n", &a[1]);
+	printf("a[2] = %p\n", &a[2]);
+	// printf("a[3] = %p\n", &a[3]);
+	// printf("a[4] = %p\n", &a[4]);
+	return (0);
 }
