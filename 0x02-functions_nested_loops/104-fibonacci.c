599dd0a47d25b329a65d9e0b151ec42b2d46d194
bakare <bakaredavid007@gmail.com>
2022-08-21 01:17:06 +0100

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,44 @@
 
 int main(void)
 {
-	unsigned long int i, j = 1, temp = 1;
+	unsigned long int i = 0, j = 1, temp;
+    unsigned long int i_1, i_2, j_1, j_2;
+    unsigned long int i_i, j_j;
 	int k = 0;
 
-	for (i = 0, k = 0; k <= 98; i++, k++)
+	for (k = 0; k < 92; k++)
 	{
-		if (k == 98)
-		{
-			printf("%ld", temp);
-			break;
-		}
-		printf("%ld, ", temp);
-		i = j;
-		j = temp;
-		temp = i + j;
+        temp = i + j;
+        printf("%ld, ",temp);
+
+        i = j;
+        j = temp;
 	}
+
+    i_1 = i / 10000000000;
+    j_1 = j / 10000000000;
+    i_2 = i % 10000000000;
+    j_2 = j % 10000000000;
+
+    for (k = 93; k < 99; k++)
+    {
+        i_i = i_1 + j_1;
+        j_j = i_2 + j_2;
+        if (i_2 + j_2 > 9999999999)
+        {
+            i_i += 1;
+            j_j %= 10000000000;
+        }
+
+        printf("%lu%lu", i_i, j_j);
+        if (k != 98)
+            printf(", ");
+        i_1 = j_1;
+        i_2 = j_2;
+        j_1 = i_i;
+        j_2 = j_j;
+
+    }
 	putchar('\n');
 	return (0);
 }
