70005528da9fc3e3e2fe7f51d91c4fa3de597ba4
bakare <bakaredavid007@gmail.com>
2022-08-25 12:28:07 +0100

    Fixed Readme

--- 0x06-pointers_arrays_strings/just.c
+++ 0x06-pointers_arrays_strings/just.c
@@ -1,48 +1,22 @@
-#include "main.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/**
- * main - magic.c
- * Return: Always (0)
- */
+void infinite_add(char *n1, char *n2, char *r)
+{
+    unsigned long long int n = atoi(n1);
+    unsigned long long int m = atoi(n2);
+    unsigned long long int z;
+    z = n + m;
+    // r = itoa(z, r, 10);
+    printf("%lld", z);
+}
 
 int main(void)
 {
-	int m;
-	int a[5];
-	int *p;
-
-    //in c when an integer is declared and an integer array follows, 6 bits of memory is given
-
-    //in c when an integer is declared and an char array follows, 15 bits of memory is given
-
-    //when a char is declared and a char array follows, 
-    //12 bits of memory is given
-
-    //when a char is declared and a int array follows, 
-    //9 bits of memory is given
-
-	// a[2] = 1024;
-	p = &m;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 4) = 98;
-	/* ...so that this prints 98\n */
-    // printf("n = %ld\n", sizeof(3));
-    printf("n = %p\n", (p + 5));
-    printf("m = %p\n", &m);
-    // printf("n = %d\n", *(p + sizeof(int) * ));
-	printf("a = %p\n", &a);
-	// printf("a[0] = %p\n", &a[0]);
-	// printf("a[1] = %p\n", &a[1]);
-	printf("a[2] = %p\n", &a[2]);
-	// printf("a[3] = %p\n", &a[3]);
-	// printf("a[4] = %p\n", &a[4]);
-	return (0);
+    char *n1 = "102";
+    char *n2 = "2";
+    char *r;
+    infinite_add(n1, n2, r);
 }
--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-Alx-low-level_programming
+# Low-Level Programming - Data Structures, Algorithms, Linux/Unix System Programming
+This repository contains programs written for the low-level programming and algorithm track at alx/Holberton School
 
-#Introduction to the c programming language 
+- 0x00.C-Hello, World
+- 0x01.C-Variables, if, else, while
+- 0x02.C-Function, nested loops
+- 0x03.C-Debugger
+- 0x04.C-More functions nested_Loops
+- 0x05.C-Pointers arrays strings
+- 0x06.C-More pointers arrays strings
+- 0x07.C-Even more Pointers arrays strings
+- 0x08.C-Recursion
+- 0x09.C-Static libraries
+- 0x10.C-Variadaic function
+- 0x14.C-Bit manipulation
+- 0x15.C-file i/o
+- 0x0A.C-argc argv
+- 0x0B.C-Memory management
+- 0x0C.C-More memory management
+- 0x0D.C-Preprocessor
+- 0x0E.C-Structures and Typedef
+- 0x0F.C-Function pointers
