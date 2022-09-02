928288bcc17b3bfdaa4bfe2a2885fabcfa7678c3
bakare <bakaredavid007@gmail.com>
2022-09-02 02:55:21 +0100

    test 1, task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints qll elements of a dlistint list
+ * print_dlistint - prints all elements of a dlistint list
  * @h: double linked list node structure
  *
  * Return: the number of nodes
@@ -9,14 +9,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int len = 0;
+	size_t len = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h != NULL)
 	{
-		len += printf("%d\n", h->n);
+		len++;
+		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (len / 2);
+	return (len);
 }
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * dlistint_len - returns the number of elements in a
+ * a doubly linked list
+ * @h: doubly linked list node structure
+ *
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
