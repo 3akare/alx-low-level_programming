e010b788137ad7af4db8aaad4c56ac645802649a
bakare <bakaredavid007@gmail.com>
2022-09-02 02:36:00 +0100

    test 1, task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_dlistint - prints qll elements of a dlistint list
+ * @h: double linked list node structure
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(dlistint_t *h)
+{
+	int len = 0;
+	while (h != NULL)
+	{
+		len += printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (len / 2);
+}
