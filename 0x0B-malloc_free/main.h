a65640be85c92d5ed42f9ad09debbcd22b212904
bakare <bakaredavid007@gmail.com>
2022-09-05 03:51:09 +0100

    test 1, task 101

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,8 @@ char *create_array(unsigned int size, char c);
 char *_strdnup(char *str);
 char *str_concat(char *s1, char *s2);
 char *argstostr(int ac, char **av);
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
 
 #endif
