8a3dbf847b96a2146808a795796a0852a04c253f
bakare <bakaredavid007@gmail.com>
2022-09-07 16:19:42 +0100

    test 2, atsk 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,18 +10,18 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w;
+	int o, w, len = 0;
 
 	if (!filename)
 		return (-1);
 	if (text_content)
-	{
-		o = open(filename, O_WRONLY | O_APPEND);
-		w = write(o, text_content, strlen(text_content) + 1);
-		if (0 == -1 || w == -1)
-			return (-1);
-		close(o);
-		return (1);
-	}
-	return (-1);
+		len = strlen(text_content);
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, strlen(text_content) + 1);
+
+	if (0 == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
 }
