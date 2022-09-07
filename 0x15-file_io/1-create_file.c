662c94bfb328d4b014b91ee130522256a22cd5b5
bakare <bakaredavid007@gmail.com>
2022-09-07 18:11:23 +0100

    test 11, task 0 - 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (FAIL);
 	if (!text_content)
 	{
-		for (len = 0; text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
 	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
