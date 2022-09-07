0ed964760840e12df8dc774e30011964f087b4ab
bakare <bakaredavid007@gmail.com>
2022-09-07 16:33:47 +0100

    test 8, task 0 - 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,17 +11,25 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buffer[letters];
+	char *buffer;
 	ssize_t o, r, w;
 
 	if (!filename)
+		return (0);
+
+	buffer = malloc(sizeof(size_t) * letters);
+	if (!buffer)
 		return (0);
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 || r != w)
+	{
+		free(buffer);
 		return (0);
+	}
+	free(buffer);
 	close(o);
 	return (w);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		len = strlen(text_content);
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len;
+	w = write(o, text_content, len);
 	if (o == -1 || w == -1)
 		return (FAIL);
 	close(o);
