21c4cdb0792c8db2372c2f3fb1ce3ae1400ee2ea
bakare <bakaredavid007@gmail.com>
2022-09-08 17:16:25 +0100

    test 7, task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,11 +18,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (!new_dog)
-        {
-                free(new_dog);
+	{
 		return (NULL);
-        }
+	}
 
+	new_dog->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (!new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->name = strdup(name);
 	new_dog->age = age;
 	new_dog->owner = strdup(owner);
