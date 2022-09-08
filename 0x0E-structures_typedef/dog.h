958cca7dda1fe6341c12bedc32c4f73e0b3ef254
bakare <bakaredavid007@gmail.com>
2022-09-08 15:44:14 +0100

    test 1, task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,13 @@
  * @age: dog member
  *
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
