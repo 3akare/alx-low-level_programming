5caa0c43a1a3ee998392a029f766891f21b246f5
bakare <bakaredavid007@gmail.com>
2022-09-14 14:17:03 +0100

    test 2, task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,12 @@
 #include "lists.h"
 
 /**
- * main - a function
+ * hare - a function
  * Return: always successful
  */
 
-int main(void)
+void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
-	printf("(A tortoise, having pretty good sense");
-	printf(" of a hare's nature, challenges one to a race.)\n");
-	return (0);
+	printf("I bore my house upon my back!\n");
 }
