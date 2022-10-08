bf01403dc7d99e66918d5a18e93eeca76f20d7ff
bakare <bakaredavid007@gmail.com>
2022-10-08 04:36:02 +0100

    test 2, task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
+	new->n = n;
 	if (!new)
 		return (NULL);
 
@@ -24,7 +25,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->next = *head;
 	(*head)->prev = new;
-	new->n = n;
 	*head = new;
 	return (*head);
 }
