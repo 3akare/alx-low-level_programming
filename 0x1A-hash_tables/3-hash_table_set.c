6c07692d32d37b8af9c70285d081f3a8f8b3010d
bakare <bakaredavid007@gmail.com>
2022-10-15 21:48:05 +0100

    test 3, task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,15 +22,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = 0; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			free(ht->array[i]->value);
-			/** ht->array[i]->value = value_copy; */
-			return (1);
-		}
-	}
+	/*
+    * for (i = 0; ht->array[i]; i++)
+	* {
+	*	if (strcmp(ht->array[i]->key, key) == 0)
+	*	{
+	*		free(ht->array[i]->value);
+	*		ht->array[i]->value = value_copy;
+	*		return (1);
+	*	}
+    * }
+	*/
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 	{
