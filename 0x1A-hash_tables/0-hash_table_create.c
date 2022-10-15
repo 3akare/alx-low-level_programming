c733eb1b4d4bc1e5027b933d4e27633f4df3207a
bakare <bakaredavid007@gmail.com>
2022-10-15 21:11:36 +0100

    test 3, task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	unsigned long int i = 0;
+	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new_table->array)
 		return (NULL);
 	for (i = 0; i < size; i++)
