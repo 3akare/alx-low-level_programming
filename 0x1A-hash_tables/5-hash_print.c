23ee135147f6b710446cc5f685d5d9ed46ec43d6
bakare <bakaredavid007@gmail.com>
2022-10-15 23:23:43 +0100

    test 1, taks 5

---
+++ 0x1A-hash_tables/5-hash_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: A hahs table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i = 0;
+	unsigned char flag = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i])
+		{
+			if (flag == 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s':'%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
