50fdd2334cceb04e6bc590bf69b83d72a68e198b
David Bakare <bakaredavid007@gmail.com>
2023-01-04 13:18:33 +0100

    description update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
- * linear_search - Searches for a vlaue in an array of integers
+ * linear_search - Searches for a value in an array of integers
  * using the Linear search algorithm
  * @array: A pointer to the first element of the array
  * @size: Is the number of elements in @array
