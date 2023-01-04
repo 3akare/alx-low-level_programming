872da8904b6502ea8c176647742a9e1f8bf2ec33
3akare <bakaredavid007@gmail.com>
2023-01-04 07:38:30 +0000

    interpolation, test 1

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,35 +1,51 @@
 #include "search_algos.h"
 
+/**
+ * interpolation_search - Searches for a value in sorted array of integers
+ * using interpolation search algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @size: is the number og elements in array
+ * @value: The value to be found
+ *
+ * Return: The first index where value is located on success, and -1 on fail
+ */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t low, high, mid, index, tracker = 0;
+	size_t low, high, mid = 0;
+	
+	low = 0;
+	high = size - 1;
+	if (array == NULL)
+		return (-1);
+	while (low <= high)
+	{
+		mid = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		if (mid < size)
+			printf("Value checked array[%ld] = [%d]\n", mid, array[mid]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", mid);
+			break;
+		}
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] > value)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return (-1);
+}
+int main(void)
+{
+    int array[] = {
+        0, 0, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
 
-    low = 0;
-    high = size - 1;
-    while (low <= high)
-    {
-        tracker++;
-        mid = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-        
-        if (array[mid] == value){
-            index = mid;
-            break;
-        }
-        else{
-            if (array[mid] < value)
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-        if (tracker > size)
-        {
-            printf("Value checked array[%ld] is out of range\n", mid);
-            return (-1);
-        }
-        else
-        {
-            printf("Value checked array[%ld] = [%d]\n", mid, array[mid]);
-        }
-    }
-    return (index);
+    printf("Found %d at index: %d\n\n", 3, interpolation_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 7, interpolation_search(array, size, 7));
+    printf("Found %d at index: %d\n", 999, interpolation_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
